e4fe931e7b2ccdb05a2ac4369db8cd47856d1a5b
Ariel <arielanus1980@yahoo.com.ar>
2022-03-24 15:03:15 -0300

    TP 3-2

---
+++ variables.js
@@ -0,0 +1,54 @@
+var base= 7
+var altura = 10
+var superficieTriangulo= (base*altura)/2;
+console.log ("La superficie del triangulo es de "+superficieTriangulo+" cm");
+
+var base= 5
+var altura = 8
+var  superficieRectangulo= (base*altura);
+console.log ("El rectangulo  tiene una superficie de "+superficieRectangulo+ " cm");
+
+const pi= 3.14
+var radio= 6
+var superficieCirculo= (pi*(radio**2))
+console.log("El circulo tiene una superfice de "+superficieCirculo+ " cm");
+
+
+var primera= 8
+var segunda= 7
+var tercera= 5
+var suma= primera+segunda+tercera;
+var producto=primera*segunda*tercera;
+var promedio=(primera+segunda+tercera)/3;
+console.log("La suma de las tres variables es de  "+suma);
+console.log("El producto de las tres variables es de "+producto);
+console.log("El promedio de las tres variables es de "+promedio);
+
+var jamonCocido= 250
+var quesoMuzzarella=170
+var quesoFontina=200
+var pastron= 350
+var salameDeMilan=280
+var compraTotal=jamonCocido+quesoMuzzarella+quesoFontina+pastron+salameDeMilan
+var descuentoSobreCompra=compraTotal*0.15
+var precioAbonadoFinal=compraTotal-descuentoSobreCompra
+console.log("El valor total de la compra fue de "+compraTotal+ " pesos");
+console.log("El descuento obtenido fue de "+descuentoSobreCompra+" pesos");
+console.log("El precio que al final se pagó es de "+precioAbonadoFinal+" pesos");
+
+var importeFactura=8500
+const alicuota=0.21
+var valorIva=importeFactura*alicuota
+console.log("El importe del IVA es de "+valorIva+" pesos");
+
+var sumaEnPesos= 250000
+var cuantosPesosCuestaComprarUnDolar= 201
+var valorEquivalenteEnDolares= (sumaEnPesos/cuantosPesosCuestaComprarUnDolar)
+console.log ("El valor equivalente en dolares de 250000 pesos es "+ valorEquivalenteEnDolares+" pesos");
+
+
+
+
+
+
+
